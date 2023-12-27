3bc8817c2930680a864ab7ffcefce6dcec9adaf2
DaniDeDos <hasashih978@gmail.com>
2023-12-27 23:33:36 +0100

    up

---
+++ README.md
@@ -0,0 +1,61 @@
+<div>
+  <img
+    src="https://capsule-render.vercel.app/api?type=rect&color=gradient&height=1"
+    width="100%"
+  />
+</div>
+
+<div>
+  <img src="/images/banner-DaniDeDos.png" width="100%" />
+</div>
+¡Revisa mi contenido 👇!
+<!--line-->
+<div>
+  <img
+    src="https://capsule-render.vercel.app/api?type=rect&color=gradient&height=1"
+    width="100%"
+  />
+</div>
+<!--header-->
+<p align="center">
+  <img
+    src="https://capsule-render.vercel.app/api?type=venom&color=gradient&height=300&text=Hola!&desc=👋"
+  />
+</p>
+
+<!--info-->
+
+🌟
+<h2>My Projects</h2>
+<a
+  href="https://github.com/DaniDeDos/react-TodoListApp"
+  target="_blank"
+  rel="noreferrer"
+>
+  <img
+    align="center"
+    alt="asd"
+    src="https://github-readme-stats.vercel.app/api/pin/?username=DaniDeDos&theme=react&repo=react-TodoListApp"
+  />
+</a>
+
+<br />
+<br />
+
+<!-- Tech Stack -->
+<details open>
+  <summary><b>🛠️ Tech Stack</b></summary>
+  <p>----</p>
+</details>
+
+<!--footer-->
+<p align="center">
+  <img
+    src="https://capsule-render.vercel.app/api?type=Waving&color=gradient&height=80&section=footer"
+    width="100%"
+  />
+</p>
+
+<p align="right"><i>ultima coneccion</i> : <b>(.*?)</b></p> 
+
+# DaniDeDos
---
+++ index.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const fs = require("fs").promises;
+
+const getLastActivityDate = async (username) => {
+ const response = await axios.get(`https://api.github.com/users/${username}/events`);
+ if (response.data && response.data.length > 0) {
+ // Find the latest event that is a PushEvent
+ const latestPushEvent = response.data.find(event => event.type === 'PushEvent');
+ if (latestPushEvent) {
+   return new Date(latestPushEvent.created_at).toLocaleString();
+ } else {
+   throw new Error("No PushEvent found for this user");
+ }
+ } else {
+ throw new Error("No events found for this user");
+ }
+};
+
+async function updateReadme() {
+ try {
+  const lastActivityDate = await getLastActivityDate("DaniDeDos");
+  let content = await fs.readFile("./readme.md.tpl", { encoding: "utf-8" });
+  const regex = /<p align="right"><i>ultima coneccion<\/i> : <b>(.*?)<\/b><\/p>/;
+  const match = content.match(regex);
+  if (match) {
+    const updatedContent = content.replace(regex, `<p align="right"><i>ultima coneccion</i> : <b>${lastActivityDate}</b></p>`);
+    await fs.writeFile("./readme.md.tpl", updatedContent, { encoding: "utf-8" });
+    console.log("Updated readme.md.tpl successfully");
+  } else {
+    console.error("Could not find the line containing the last activity");
+  }
+ } catch (error) {
+  console.error("An error occurred:", error);
+ }
+}
+
+updateReadme();
