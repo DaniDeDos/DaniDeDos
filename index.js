e3c98a67b9858bfa6ac717b1f758819b8baa3c01
Daniel Arrechea <151276855+DaniDeDos@users.noreply.github.com>
2023-12-29 21:21:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,37 @@ async function main() {
  await updateBotStatus("Online");
 }
 
+const fs = require('fs').promises;
+
+async function readFile() {
+   try {
+       const data = await fs.readFile('./README.md', 'utf8');
+       // procesar los datos aquí
+   } catch (err) {
+       console.error(`Error reading file: ${err}`);
+   }
+}
+
+readFile();
+function generateProgressBar(percentage) {
+   const progressBarCapacity = 30;
+   const passedProgressBarIndex = parseInt(percentage * progressBarCapacity);
+   const progressBar = '█'.repeat(passedProgressBarIndex) + ' '.repeat(progressBarCapacity - passedProgressBarIndex);
+   return `{ ${progressBar} }`;
+}
+
+
+async function writeFile(data) {
+   try {
+       await fs.writeFile('./README.md', data, 'utf8');
+       console.log('File updated successfully');
+   } catch (err) {
+       console.error(`Error writing file: ${err}`);
+   }
+}
+
+writeFile(updatedData);
+
+
+
 main();
