35530495c195303be530faffa4f0a2fdbceb68b6
Daniel Arrechea <151276855+DaniDeDos@users.noreply.github.com>
2023-12-29 07:32:23 +0100

    Update updateLastConnection.js

--- updateLastConnection.js
+++ updateLastConnection.js
@@ -6,34 +6,35 @@ export async function updateLastConnection(username) {
  const lastActivityDate = await getLastActivityDate(username);
  let content = await fs.readFile("./README.md", { encoding: "utf-8" });
  const regex1 = /<p align="right"><i>ultima coneccion<\/i> : <b>(.*?)<\/b><\/p>/;
- const match = content.match(regex1);
- if (match) {
-   const updatedContent = content.replace(regex1, `<p align="right"><i>ultima coneccion</i> : <b>${lastActivityDate}</b></p>`);
-   await fs.writeFile("./README.md", updatedContent, { encoding: "utf-8" });
-   console.log("Actualizado readme.md con éxito");
+ const match1 = content.match(regex1);
+ if (match1) {
+  const updatedContent = content.replace(regex1, `<p align="right"><i>ultima coneccion</i> : <b>${lastActivityDate}</b></p>`);
+  await fs.writeFile("./README.md", updatedContent, { encoding: "utf-8" });
+  console.log("Actualizado readme.md con éxito");
  } else {
-   console.error("No se pudo encontrar la línea que contiene la última actividad");
+  console.error("No se pudo encontrar la línea que contiene la última actividad");
  }
 
  // Actualiza el estado del bot
  let botStatus = "Offline";
  if (lastActivityDate) {
-   botStatus = "Online";
+  botStatus = "Online";
  }
  content = await fs.readFile("./README.md", { encoding: "utf-8" });
  const regex2 = /<p>Bot activo: .*?<\/p>/;
- const match = content.match(regex2);
- if (match) {
-   const updatedContent = content.replace(regex2, `<p>Bot activo: ${botStatus}</p>`);
-   await fs.writeFile("./README.md", updatedContent, { encoding: "utf-8" });
-   console.log("Actualizado readme.md con éxito");
+ const match2 = content.match(regex2);
+ if (match2) {
+  const updatedContent = content.replace(regex2, `<p>Bot activo: ${botStatus}</p>`);
+  await fs.writeFile("./README.md", updatedContent, { encoding: "utf-8" });
+  console.log("Actualizado readme.md con éxito");
  } else {
-   console.error("No se pudo encontrar la línea que contiene el estado del bot");
+  console.error("No se pudo encontrar la línea que contiene el estado del bot");
  }
  } catch (error) {
  console.error("Ocurrió un error:", error);
  }
 }
+
 
 async function getLastActivityDate(username) {
  const response = await axios.get(`https://api.github.com/users/${username}/events`);
