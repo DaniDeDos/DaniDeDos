8cf2b8cd2664cbfa0d6808dfcf34cca11d164f4b
Daniel Arrechea <151276855+DaniDeDos@users.noreply.github.com>
2023-12-29 07:23:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import axios from "axios";
 import fs from 'fs/promises';
 import { updateLastConnection } from './updateLastConnection.js';
 
+
+async function updateBotStatus(status) {
+ const data = { botStatus: status };
+ await fs.writeFile('./status.json', JSON.stringify(data));
+}
+
 async function main() {
  // Actualiza README.md con la última fecha de actividad
  await updateLastConnection("DaniDeDos");
+ // Actualiza el estado del bot
+ await updateBotStatus("Online");
 }
-
 
 main();
