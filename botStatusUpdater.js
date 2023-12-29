07edb5487c2f6cd05943eb0f69547ba696a4f9d8
Daniel Arrechea <151276855+DaniDeDos@users.noreply.github.com>
2023-12-29 06:39:54 +0100

    Update botStatusUpdater.js

--- botStatusUpdater.js
+++ botStatusUpdater.js
@@ -7,10 +7,12 @@ async function isBotActive(username) {
  const response = await axios.get(`https://api.github.com/users/${username}/events`);
  let botActive = 'Offline';
  if (response.data && response.data.length > 0) {
-   botActive = 'Online';
+  botActive = 'Online';
  }
  // Actualizar el estado del bot en el archivo status.json
- await fs.writeFile("./status.json", JSON.stringify({ botStatus: botActive }), { encoding: "utf-8" });
+ const botStatus = JSON.parse(await fs.readFile("./status.json", 'utf8'));
+ botStatus.botStatus = botActive;
+ await fs.writeFile("./status.json", JSON.stringify(botStatus), { encoding: "utf-8" });
 }
 
 async function setBotActiveState(state) {
