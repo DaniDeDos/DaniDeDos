a52b656d27883cac6dc8534de498d27f10a7e84b
Daniel Arrechea <151276855+DaniDeDos@users.noreply.github.com>
2023-12-29 08:01:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,20 @@ async function main() {
  await updateBotStatus("Online");
 }
 
+async function updateBotStatus(status) {
+ const data = { botStatus: status };
+ await fs.writeFile('./status.json', JSON.stringify(data));
+
+ // Leer el contenido de README.md
+ let content = await fs.readFile("./README.md", { encoding: "utf-8" });
+
+ // Buscar la URL de la imagen del escudo y reemplazar el valor de "botStatus"
+ const regex = /https:\/\/img\.shields\.io\/badge\/GitHub%20Action%20Status-([^-\/]*)-brightgreen\?style=flat&logo=githubactions&logoColor=%23ffffff&labelColor=%23181717&color=%232088FF/;
+ const updatedUrl = content.replace(regex, `https://img.shields.io/badge/GitHub%20Action%20Status-${status}-brightgreen?style=flat&logo=githubactions&logoColor=%23ffffff&labelColor=%23181717&color=%232088FF`);
+
+ // Escribir el contenido actualizado de nuevo a README.md
+ await fs.writeFile("./README.md", updatedUrl, { encoding: "utf-8" });
+}
+
+
 main();
