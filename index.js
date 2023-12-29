62e7d4903afe7f0a4c156038893b9842e7d4ca7b
Daniel Arrechea <151276855+DaniDeDos@users.noreply.github.com>
2023-12-30 00:03:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,18 +47,16 @@ function getPartOfDay(commit) {
 async function countCommitsByPartOfDay(username) {
  const commits = await getCommits(username);
  const counts = {
-  morning: 0,
-  afternoon: 0,
-  evening: 0,
-  night: 0
+   morning: 0,
+   afternoon: 0,
+   evening: 0,
+   night: 0
  };
 
  commits.forEach(commit => {
-  const date = moment(commit.created_at).local().format('YYYY-MM-DD HH:mm:ss');
-  const hour = moment(date).hour();
-  const partOfDay = getPartOfDay(hour);
-  counts[partOfDay]++;
-  console.log(`Counting commit for ${partOfDay}: ${commit.id}`);
+   const partOfDay = getPartOfDay(commit.hour);
+   counts[partOfDay]++;
+   console.log(`Counting commit for ${partOfDay}: ${commit.id}`);
  });
 
  console.log(`Final counts: ${JSON.stringify(counts)}`);
